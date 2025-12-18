74b4b572026b7a8bf3fbb617b563c856d1780d4f
Shraddha Shukla <shraddhashukla327@gmail.com>
2025-12-18 12:49:08 +0530

    Add C# project files and Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of elements in Array:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the elements of Array:");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter Threshold:");
+        int threshold = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Filtered Array (elements greater than Threshold):");
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] > threshold)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
